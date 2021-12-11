6bbeed895570c10266185e554dca52547662fc02
Anamika <anamikarecsonbhadra@gmail.com>
2021-12-11 14:04:28 +0530

    react-credit-card-form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Form from "./Component/Form"
+import CreditCard from "./Component/CreditCard"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Credit Card Form</h1>
+      <div style={{display:"flex",padding:"10px",justifyContent:"center"}}>
+      <div style={{marginRight:"100px",marginTop:"70px"}}><CreditCard /></div>
+      <div> <Form /></div>
+      </div>
+     
     </div>
   );
 }
